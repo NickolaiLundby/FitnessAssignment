d63658ec2d95cbfcf9d92ecd9f07691878223c36
bertflemming <tobiasniklassen@gmail.com>
2019-09-23 12:34:45 +0200

    Fixed layout + login.pug + users route

--- Group15_FitnessAssignment/routes/users.js
+++ Group15_FitnessAssignment/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var ctrlUsers = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/login', ctrlUsers.showLogin);
+router.post('/login', ctrlUsers.login);
+router.get('/register', ctrlUsers.showRegister);
+router.post('/register', ctrlUsers.Register);
+
 
 module.exports = router;
