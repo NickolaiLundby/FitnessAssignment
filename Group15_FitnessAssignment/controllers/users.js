0703cb1975fb68d474448bb6c203d1bed07b6f7c
bertflemming <tobiasniklassen@gmail.com>
2019-09-23 13:12:29 +0200

    controller

--- Group15_FitnessAssignment/controllers/users.js
+++ Group15_FitnessAssignment/controllers/users.js
@@ -1,1 +1,20 @@
-'use strict';
+const userModel = require('../models/user');
+const passport = require('passport');
+
+module.exports.showLogin = function (req, res, next) {
+    res.render('login', { title: 'Login' });
+    };
+
+module.exports.login = function (req, res, next) {
+    /*passport.authenticate('local', { failureRedirect: '/login' }, function(req, res) {
+        res.redirect('/')
+    });*/
+    };
+
+module.exports.showRegister = function (req, res, next) {
+    res.render('register', { title: 'Register' });
+};
+
+module.exports.register = function (req, res, next) {
+   // userModel.createUser(req, res);
+};
--- Group15_FitnessAssignment/routes/users.js
+++ Group15_FitnessAssignment/routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var ctrlUsers = require('../controllers/users');
+const ctrlUsers = require('../controllers/users');
 
 /* GET users listing. */
 
 router.get('/login', ctrlUsers.showLogin);
 router.post('/login', ctrlUsers.login);
 router.get('/register', ctrlUsers.showRegister);
-router.post('/register', ctrlUsers.Register);
+router.post('/register', ctrlUsers.register);
 
 
 module.exports = router;
