175ea969b8a1784b02b402df722b06e949fc3c36
poxbabs <nickolai.ernst@hotmail.com>
2019-09-23 13:27:55 +0200

    Bootstrapping

---
+++ Group15_FitnessAssignment/config/express.js
@@ -0,0 +1,28 @@
+// Module dependencies
+
+const express = require('express');
+//const session = require('express-session');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+module.exports = function(app, passport){
+    // Setup database here
+
+    // View engine setup
+    app.set('views', path.join(__dirname + '/../', 'views'));
+    app.set('view engine', 'pug');
+
+    // Static files
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    
+
+    // Passport for authentication
+    app.use(passport.initialize());
+    //app.use(passport.session());
+}
---
+++ Group15_FitnessAssignment/controllers/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Module dependencies
+
+exports.index = function(req, res) {
+    res.render('index', {title: 'PUG' });
+}
