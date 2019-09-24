1217c5700a95ebd2dd7c57f1d5184094a90edbb8
Nickolai Lisberg Lundby <35376319+NickolaiLundby@users.noreply.github.com>
2019-09-24 13:32:25 +0200

    Logout logic (#9)
    * Logout logic
    
    * Set engines in package.json - NEeded for Heroku
    
    * Procfile for Heroku

--- Group15_FitnessAssignment/config/routes.js
+++ Group15_FitnessAssignment/config/routes.js
@@ -19,6 +19,7 @@ module.exports = function(app, passport) {
         });
     app.get('/register', users.showRegister);
     app.post('/register', users.register);
+    app.get('/logout', users.logout);
 
     // Workout routes
 
